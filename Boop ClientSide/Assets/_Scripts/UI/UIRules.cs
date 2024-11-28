b3b8ca0227b6eabb244162181341178e770a1e8e
Alexis Bourgois <bourgoisalexis@gmail.com>
2024-11-28 11:45:18 +0100

    Reimplementing rules to make modifs easier

--- Boop ClientSide/Assets/_Scripts/UI/UIRules.cs
+++ Boop ClientSide/Assets/_Scripts/UI/UIRules.cs
@@ -2,22 +2,34 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Text;
+
+[Serializable]
+public class Rule {
+    public int start;
+    public int end;
+}
 
 public class UIRules : MonoBehaviour {
+    #region Variables
     [SerializeField] private GameObject _view;
     [SerializeField] private Image _background;
     [SerializeField] private TextMeshProUGUI _title;
     [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private TextAsset _rulesAsset;
+    [SerializeField] private List<Rule> _rules = new List<Rule>();
+
     [Header("Buttons")]
     [SerializeField] private UIButton _showButton;
     [SerializeField] private UIButton _closeButton;
     [SerializeField] private UIButton _nextButton;
     [SerializeField] private UIButton _previousButton;
 
-    [Multiline(10)]
-    [SerializeField] private List<string> _rules = new List<string>();
-
     private int _currentIndex;
+    [SerializeField] private List<string> _actualRules = new List<string>();
+    #endregion
+
 
     public void Init() {
         _currentIndex = 0;
@@ -30,7 +42,21 @@ public class UIRules : MonoBehaviour {
         _nextButton.onClick.AddListener(Next);
         _previousButton.onClick.AddListener(Previous);
 
+        GetRules();
         Hide();
+    }
+
+    private void GetRules() {
+        string[] lines = _rulesAsset.text.Split('\n');
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var rule in _rules) {
+            sb.Clear();
+            for (int i = rule.start - 1; i < rule.end; i++)
+                sb.AppendLine(lines[i]);
+
+            _actualRules.Add(sb.ToString());
+        }
     }
 
     private void Show() {
@@ -55,6 +81,6 @@ public class UIRules : MonoBehaviour {
     private void UpdateView() {
         _previousButton.gameObject.SetActive(_currentIndex > 0);
         _nextButton.gameObject.SetActive(_currentIndex < _rules.Count - 1);
-        _text.text = _rules[_currentIndex].ToString();
+        _text.text = _actualRules[_currentIndex];
     }
 }
